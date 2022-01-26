26b62a0fc9f5e0e9b2a982a74fee15acd4a72aee
yair-hb <aythb@hotmail.com>
2022-01-26 13:44:00 -0600

    se agrega el entrenamiento de modelos de reconocimiento de rostros

--- TrainingRF.py
+++ TrainingRF.py
@@ -27,5 +27,21 @@ print ('etiquetas= ', Etiquetas)
 print ('Numero de etiquetas 0 :', np.count_nonzero(np.array(Etiquetas)==0))
 print ('Numero de etiquetas 1 :', np.count_nonzero(np.array(Etiquetas)==1))
 
+#elegimos el metodo de entrenamiento para elreconocimiento de rostros
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
+#face_recognizer = cv2.face.FisherFaceRecognizer_create()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+print ('Entrenando Metodo...')
+print ('....................')
+print ('....................')
+print ('....................')
+face_recognizer.train(facesData, np.array(Etiquetas))
+
+#almacenamos el modelo del entrenamiento creado
+#face_recognizer.write ('modeloEigenFace.xml')
+#face_recognizer.write ('modeloFisherFace.xml')
+face_recognizer.write ('modeloLBPHFace.xml')
+print ('Modelo Almacenado con exito!!!')
 
 
